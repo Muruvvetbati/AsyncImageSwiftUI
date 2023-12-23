9776eddaf1ef5a7fc692e9d88de14b675d203d36
Muruvvetbati <batimuruvvet@gmail.com>
2023-12-23 11:58:46 +0300

    Initial Commit

---
+++ AsyncSwiftUI/AsyncSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncSwiftUIApp.swift
+//  AsyncSwiftUI
+//
+//  Created by Devmuruvvet on 23.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct AsyncSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
